3a83d75ccad6f7a1ceb12d5d5e805f6406d4ca06
Shota Sato <last11142@gmail.com>
2023-07-01 16:57:21 +0900

    [update] docsの更新
    加えて，簡単なtestを書いてみたよ

--- test/controllers/easy_api_controller_test.rb
+++ test/controllers/easy_api_controller_test.rb
@@ -4,4 +4,10 @@ class EasyApiControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get easy_api" do
+    get easy_api_url
+    assert_response :success
+  end
+
 end
